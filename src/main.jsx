b62f02233fec4c5f35a370344941ecc2e35b38af
nico-hondo <nicohondo01@gmail.com>
2026-08-14 09:12:11 +0700

    feat: add sign-up and forgot-pass page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import SignIn from './pages/auth/SignIn'
+// import SignIn from './pages/auth/SignIn'
+// import SignUp from './pages/auth/SignUp'
+import ForgotPass from './pages/auth/ForgotPass'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SignIn />
+    <ForgotPass />
   </StrictMode>,
 )
